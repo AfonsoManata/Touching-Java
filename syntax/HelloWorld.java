0db6f51c4796c9696cbc02a56b99d65d0bbd111f
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-07-17 08:37:08 +0100

    Java

--- syntax/HelloWorld.java
+++ syntax/HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+		// Data types in Java
+
+		// Primitive Data types
+		int number = 10;
+		double price = 9.99;
+		char grade = 'A';
+		boolean isJavaFun = true;
+
+		//Non-Primitive Data types
+		String greeting = "Hello"
+		int[] numbers = {1, 2, 3, 4, 5};
+
+		int greetLenght = greeting.length();
+
+		System.out.println(greetLenght);
     }
 }
